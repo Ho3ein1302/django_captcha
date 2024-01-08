ef003512762bd7acdffe319ce5ab577e4b2b4922
Shinigami1302 <hosseinfatehi1302@gmail.com>
2024-01-08 19:45:34 +0330

    Add user urls

--- config/urls.py
+++ config/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
-    path('captcha/', include('apps.captcha.urls'))
+    path('captcha/', include('apps.captcha.urls')),
+    path('', include('apps.users.urls')),
 ]
