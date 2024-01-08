13208e0535629a4200fd9784728613624b94e509
Shinigami1302 <hosseinfatehi1302@gmail.com>
2024-01-08 17:23:38 +0330

    Add user model

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.hashers import make_password
+from django.utils.translation import gettext as _
 
-# Create your models here.
+from .validators import check_phone
+
+
+class Users(AbstractUser):
+    phone = models.CharField(db_index=True, unique=True, validators=[check_phone], verbose_name=_('phone'))
+
+    def clean(self):
+        self.password = make_password(self.password)
+        super().clean()
