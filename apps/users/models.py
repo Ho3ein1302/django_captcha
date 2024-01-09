63a7e096df4dea486a1784bebf152301726ab699
Shinigami1302 <hosseinfatehi1302@gmail.com>
2024-01-09 08:54:05 +0330

    Modify

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,6 +1,5 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
-from django.contrib.auth.hashers import make_password
 from django.utils.translation import gettext as _
 
 from .validators import check_phone
