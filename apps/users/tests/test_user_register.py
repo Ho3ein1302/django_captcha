bc5553d02472dd109bae5a30678d97cf8f2212dc
Shinigami1302 <hosseinfatehi1302@gmail.com>
2024-01-22 13:54:36 +0330

    Add test user register

---
+++ apps/users/tests/test_user_register.py
@@ -0,0 +1,48 @@
+from django.contrib.auth import get_user_model
+from django.conf import settings
+
+from rest_framework.test import APITestCase
+from rest_framework.reverse import reverse
+from rest_framework import status
+from model_bakery import baker
+
+from .. import models
+
+
+class UserRegisterTest(APITestCase):
+    def setUp(self):
+        url = reverse('captcha:generate_captcha')
+        response = self.client.post(url, {}, follow=True)
+        captcha_key = response.data['captcha']['CAPTCHA_KEY']
+        captcha_value = settings.REDIS_CAPTCHA.get(captcha_key)
+        captcha_value = captcha_value.decode('utf-8')
+        self.url = reverse('users:user_register')
+        self.data = {
+            'first_name': 'hossein',
+            'last_name': 'fatehi',
+            'username': 'ho3ein1302',
+            'phone': '09134575075',
+            'email': 'hosseinfatehi1302@gmail.com',
+            'password': 'password123',
+            're_password': 'password123',
+            'captcha_key': captcha_key,
+            'captcha_value': captcha_value,
+        }
+
+    def test_user_register_success(self):
+        response = self.client.post(self.url, self.data)
+        self.assertEquals(response.status_code, status.HTTP_201_CREATED)
+        self.assertGreaterEqual(models.User.objects.all().count(), 1)
+
+    def test_user_register_invalidate_captcha(self):
+        self.data['captcha_value'] = 331
+        response = self.client.post(self.url, self.data)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertGreaterEqual(models.User.objects.all().count(), 0)
+
+    def test_user_register_invalidate_re_password(self):
+        self.data['re_password'] = '12345677'
+        response = self.client.post(self.url, self.data)
+        self.assertEquals(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertGreaterEqual(models.User.objects.all().count(), 0)
+
--- apps/users/urls.py
+++ apps/users/urls.py
@@ -2,10 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'users'
+
 urlpatterns = [
     path('users/', views.UserView.as_view(), name='users_list'),
-    path('register/', views.UserRegister.as_view()),
-    path('login/', views.UserLogin.as_view()),
-    path('logout/', views.UserLogOut.as_view()),
-    path('refresh/', views.GetNewRefreshToken.as_view()),
+    path('register/', views.UserRegister.as_view(), name='user_register'),
+    path('login/', views.UserLogin.as_view(), name='user_login'),
+    path('logout/', views.UserLogOut.as_view(), name='user_logout'),
+    path('refresh/', views.GetNewRefreshToken.as_view(), name='get_new_refresh_token'),
 ]
